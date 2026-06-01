91b9a213a3e67f44721f6627fe39126a476a4ace
rakhicc <rakhicc29gmail.com>
2026-06-01 14:41:16 +0300

    changes for connecting to azure sql server

---
+++ src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.example.controller;
+
+import com.example.entity.User;
+import com.example.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserRepository repo;
+
+    public UserController(UserRepository repo) {
+        this.repo = repo;
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        return repo.save(user);
+    }
+
+    @GetMapping
+    public List<User> getAll() {
+        return repo.findAll();
+    }
+}
